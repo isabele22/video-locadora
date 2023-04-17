a0a4f7ebc249351b90900ab6fa7fdbf6e4440846
isabele22 <108808972+isabele22@users.noreply.github.com>
2023-04-17 12:04:36 -0300

    Add files via upload

---
+++ videoLocadora/Atividade1/javascript.js
@@ -0,0 +1,17 @@
+const inTitle = document.getElementById("inTitle");
+const inDuracation = document.getElementById("inDuracation");
+const ouTtitle = document.getElementById("ouTtitle");
+const outDuracao = document.getElementById("outDuracao");
+const buttonConverter = document.getElementById("buttonConverter");
+
+converter.addEventListener("click", () => {
+    let title = inTitle.value;
+    let duraction = Number(inDuracation.value);
+  
+    let hours = Math.floor(duraction / 60);
+    let minutos
+    
+
+    
+})
+
